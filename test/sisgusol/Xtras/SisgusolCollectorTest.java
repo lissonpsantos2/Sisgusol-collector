4d25fd86dd1533f4bdee9d58ff6f2dc98da36857
Alisson Pereira <lissonpsantos2@gmail.com>
2016-05-16 17:06:13 -0300

    Adição e Correção dos testes

--- test/sisgusol/Xtras/SisgusolCollectorTest.java
+++ test/sisgusol/Xtras/SisgusolCollectorTest.java
@@ -45,8 +45,6 @@ public class SisgusolCollectorTest {
         System.out.println("main");
         String[] args = null;
         SisgusolCollector.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
